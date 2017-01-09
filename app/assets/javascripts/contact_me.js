d1f4670b66dfbf842d73eb4dcd855775946f1432
kboygit <heybisayangdako@gmail.com>
2017-01-09 12:40:42 -0800

    added comment to be modified for contact form

--- app/assets/javascripts/contact_me.js
+++ app/assets/javascripts/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "././mail/contact_me.php", // will redefine this part for the contact form
                 type: "POST",
                 data: {
                     name: name,
